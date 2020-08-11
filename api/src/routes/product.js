a9f18095851cd9810cf92144862ca5a7a3c8b68f
matu2209 <matu.2209@gmail.com>
2020-08-11 20:55:46 -0300

    Se realizo ruta GET /product y GET /product?search

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,33 @@
 const server = require('express').Router();
 const { Product } = require('../db.js');
 
+ 
 server.get('/', (req, res, next) => {
+
+	if (!req.query){
+		Product.findAll({
+			where:{
+				name:{
+					[Op.like]: '%'+req.query.search+'%'
+				} 
+
+			}
+		}).then (products => {
+			if (!products){
+				res.status(404).send("No se encontro el producto");
+			} else {
+				res.send(products);
+			}
+		})		
+	} else {
 	Product.findAll()
 		.then(products => {
 			res.send(products);
 		})
 		.catch(next);
+	}
 });
 
 module.exports = server;
+
+
