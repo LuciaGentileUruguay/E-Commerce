09257c63d286f49bb4766e40812c4278acbe9f57
Mauro <mauropicatto@gmail.com>
2020-08-12 12:59:28 -0300

    se crearon los componentes Product, Products y Landing; se hizo el ruteo de los comopnentes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Products from './components/Products'
+import Landing from './components/Landing'
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route 
+      exact path='/'
+      component={Landing} />
+            <Route 
+      exact path='/products'
+      component={Products} />
     </div>
+    
   );
 }
 
