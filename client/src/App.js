985f764bf92ac65ea2718d075ae15006330c8859
unknown <lucia.gentile@gmail.com>
2020-08-13 19:04:20 -0300

    Subimos NavBar con SearchBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Products from './components/Products.js';
 import FormProduct from './components/FormProduct.js';
 import FormCategories from './components/FormCategories.js'
-//import SearchBar from './components/SearchBar.js';
+import Nav from './components/Nav.js';
 import Landing from './components/Landing.js';
 import {Route} from 'react-router-dom';
+import axios from 'axios';
 
 
 //Definicion de props
 
 function App(props) {
 
+  const [products, setProducts] = useState([]);
+
+  function onSearch(product) {
+      //Llamado a la API del clima
+      axios(`http://localhost:3001/products?search=${product}`)
+        .then(res => {
+          console.log(res.data);
+          setProducts({
+            productos: res.data
+          })
+        })
+    }
+
   return (
     <div>
+      <Route path='/' render={() => <Nav onSearch={onSearch}/>}/>
       <Route exact path='/'component={Landing} />
       <Route exact path='/products' component={Products} />
       <Route exact path='/FormProduct'component={FormProduct} />
       <Route exact path='/FormCategories'component={FormCategories} />
-    {/*      <Route exact='/SearchBar'component={SearchBar} /> */}
     </div>
 
   );
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import './searchbar.css';
+import React, { useState } from "react";
 
-
-export default function SearchBar(props) {
+export default function SearchBar({onSearch}) {
+  const [products, setProducts] = useState("");
 
   return (
-    <div className={container}>
-      <input className={input} type="text" placeholder="Productos..." />
-      <button className = {btnSearch} onClick={() => props.onSearch("Buscando...")}>Buscar</button>
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      onSearch(products);
+      setProducts('');
+    }}>
+      <input
+        type="text"
+        placeholder="Productos..."
+        value={products}
+        onChange={e => setProducts(e.target.value)}
+      />
+      <input type="submit" value="Buscar" />
+    </form>
+  );
+};
+
+
+/*
+  return (
+    <div>
+      <input type="text" placeholder="Productos..." />
+      <button onClick={() => onSearch(e.target.value)}>Buscar</button>
     </div>
   )
-};
+);*/
