646dfbff3ff564401c145a3ea1def5bca4c1b9d2
unknown <lucia.gentile@gmail.com>
2020-08-13 10:18:12 -0300

    Se sube el form de productos

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-
 // import logo from './logo.svg';
 import './App.css';
-import Products from './components/Products'
-import Landing from './components/Landing'
-import {Route} from 'react-router-dom'
+import Products from './components/Products.js';
+import FormProduct from './components/FormProduct.js';
+//import SearchBar from './components/SearchBar.js';
+import Landing from './components/Landing.js';
+import {Route} from 'react-router-dom';
 
 
 //Definicion de props
 
 function App(props) {
-  return (
 
+  return (
     <div>
-      <Route 
-      exact path='/'
-      component={Landing} />
-            <Route 
-      exact path='/products'
-      component={Products} />
+      <Route exact path='/'component={Landing} />
+      <Route exact path='/products' component={Products} />
+      <Route exact path='/FormProduct'component={FormProduct} />
     </div>
-    
+
   );
 }
 
--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './product.css'
+import './product.css';
 
 
 export default function Product(props){
     return (
         <div class="card">
             <div>
-                <img class="foto"src="https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <img class="foto" src="https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
             </div>
             <div>
                 <h3>Ensalada Rigoletto</h3>
@@ -15,4 +15,3 @@ export default function Product(props){
         </div>
     )
 }
-
