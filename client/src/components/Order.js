b522e6c0a288e435f58fa217debd2501dba9bafc
sofilago <sofia.lago.93@gmail.com>
2020-08-25 00:07:15 -0300

    Sweet alerts y botones

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -2,17 +2,30 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { increment,decrement,removeProductFromCart, getProductsCart} from "../actions/index";
 import { Link } from 'react-router-dom';
-
-
+import swal from 'sweetalert';
 
 export class Order extends Component {
 
+  mostrarAlerta(indice) {
+    swal ({
+      title: "¿Desea eliminar este producto?",
+      icon: "warning",
+      buttons: ["No" ,  "Si!"],
+      dangerMode: true,
+    }).then(willDelete =>{
+      if(willDelete){
+        this.props.removeProductFromCart(this.props.match.params.id, this.props.order.products[indice].id)
+        swal({text: "El articulo se elimino correctamente eliminado", icon: "success"})
+        return;
+      }
+    });
+  }
 
   componentDidMount(){
     const { match: { params: { id }}} = this.props; //id del user
     this.props.getProductsCart(id);
-
   }
+
 
   calculoTotal (products) {
     var totalDeOrden = 0;
@@ -31,11 +44,11 @@ export class Order extends Component {
             this.props.order.products && this.props.order.products.map((el,i) => (
               <div>
                 <Link to = {'/products/' + el.id}><h4>{el.name}</h4></Link>
-                <button onClick={() => this.props.removeProductFromCart(this.props.match.params.id, el.id)}> X </button>
                 <h5>IMAGEN</h5>
                 <h5>Precio $ {el.order_line.price}</h5>
                 <h5>Total $ {el.order_line.price * el.order_line.cantidad}</h5>
                 <h5>Cantidad {el.order_line.cantidad}</h5>
+                <button onClick={() => this.mostrarAlerta(i)}> X </button>
                 <button onClick={() => this.props.decrement(this.props.match.params.id, el.id)}>-</button>
                 <button onClick={() => this.props.increment(this.props.match.params.id, el.id)}>+</button>
               </div>
