af2679b6939e383677dd328a880717f60c2ad7b6
sofilago <sofia.lago.93@gmail.com>
2020-08-26 00:28:59 -0300

    Botones, alertas y estilo

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './landing.css'
 
 export default function Landing () {
-    return (
+ return (
       <div>
         <h1 className='amaticSC title texto-tierra bold'> Universo Verde </h1>
         <img className="fotoLanding" src='http://www.hdfondos.eu/pictures/2016/0615/1/love-tree-love-romance-white-flowers-grass-sky-hearts-trees-green-fields-pics-766530.jpg' />
-      </div>
+     </div>
     )
 }
