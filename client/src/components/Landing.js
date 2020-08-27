ac1de148cba2f0679e165a8ab774adf9b3fba25a
unknown <lucia.gentile@gmail.com>
2020-08-27 10:19:16 -0300

    Se arregla formato categorias, falta aun resolver algunas cosas

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,7 +4,7 @@ import './landing.css'
 export default function Landing () {
  return (
       <div>
-        <h1 className='amaticSC title texto-tierra bold'> Universo Verde </h1>
+        <h1 className='amaticSC titleLanding texto-tierra bold'> Universo Verde </h1>
         <img className="fotoLanding" src='http://www.hdfondos.eu/pictures/2016/0615/1/love-tree-love-romance-white-flowers-grass-sky-hearts-trees-green-fields-pics-766530.jpg' />
      </div>
     )
