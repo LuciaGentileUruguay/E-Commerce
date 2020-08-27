a9b427d504ccf220ea3304049ce949cff985c91a
unknown <lucia.gentile@gmail.com>
2020-08-27 18:40:25 -0300

    Se agrega carrousell al landing

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -10,10 +10,10 @@ export default function Product(props){
             <div class="card col-3">
               <img className= "foto" src="https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" className="card-img-top" />
                 <div class="card-body">
-                  <h5 class="card-title title">{props.name}</h5>
-                  <p class="card-text text">Precio $ {props.price}</p>
+                  <h5 class="card-title title texto-tierra">{props.name}</h5>
+                  <p class="card-text text texto-tierra">Precio $ {props.price}</p>
                   <Link to={`/products/${props.id}`}>
-                  <span class="btn btn-outline-success botonDetalle">Detalle</span>
+                  <span class="btn btn-outline-success botonDetalle ">Detalle</span>
                   </Link>
                 </div>
             </div>
