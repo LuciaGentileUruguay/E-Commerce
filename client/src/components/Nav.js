71c9061d59bce12098ae4af3d70558e632f768bf
sofilago <sofia.lago.93@gmail.com>
2020-08-27 23:48:32 -0300

    SearchBar + NavBar Modificaciones

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -12,7 +12,8 @@ export class Nav extends Component {
   }
   render(){
     return (
-        <nav id="navigation" className = 'fixed top right navigation shadowsIntoLight'>
+        <nav id="navigation" className = 'fixed top right navigation shadowsIntoLight navBar'>
+          <div id="margen">
           <Link to='/'>
             <span id="navigation">
                Inicio
@@ -29,14 +30,14 @@ export class Nav extends Component {
             </svg></span>
           </Link>
 
-          <Link to='/login'>
+            <Link to = "/categories">
+              <span id="navigation">Categorias</span>
+            </Link>
+
+            <Link to='/login'>
             <span id="navigation">
               Login
             </span>
-          </Link>
-
-          <Link to = "/categories">
-            <span id="navigation">Categorias</span>
           </Link>
 
           <Link to = "/admin" onClick={()=>this.props.setAdmin(true) }>
@@ -51,8 +52,10 @@ export class Nav extends Component {
             <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
             </svg> </span>
           </Link>
+          </div>
 
           <Route exact path='/products' render={() => <SearchBar onSearch={this.props.onSearch}/>}/>
+          
         </nav>
       )}
   }
