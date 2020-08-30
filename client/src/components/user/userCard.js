6be748a45f007031a10f9a56230bf5077dd6d81c
matu2209 <matu.2209@gmail.com>
2020-08-30 20:11:23 -0300

    rodri haz lo tuyo

--- client/src/components/user/userCard.js
+++ client/src/components/user/userCard.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import Axios from 'axios';
+
 
 
 //se importo Link de react-router-dom
 export default function User(props){
 
+  function borrar (e){
+    Axios.delete("http://localhost:3001/admin/"+props.id,{withCredentials:true})
+    .then(res=>{
+      alert(res);
+    })
+  }
+
+  function administrador(){
+    Axios.put("http://localhost:3001/admin/isAdmin/"+props.id,{withCredentials:true})
+    .then(res=>{
+      alert(res);
+    })
+  }
+
 // se agrego el link to correctament, mirar app para la ruta
     return (
-      <Link to={`/users/${props.id}`}>
+      
             <div className="divroot">
               <div className = "container">
                 <h5 className = "texto">User Id:{props.id}</h5>
+                <Link to={`/users/${props.id}`}>
                 <p className = "p"> Usuario:{props.nombre+" "+props.apellido}</p>
+                </Link>
                 <p className = "p"> Email:{props.email}</p>
+                <button onClick={(e)=>borrar(e)}>X</button>
+                <button>Reset Password</button>
+                <button onClick={()=>administrador()}>Administrador</button>
               </div>
             </div>
-            </Link>  
+              
        
     )
 }
