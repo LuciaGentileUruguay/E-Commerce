220157e66218361fcd820a897bcafbafd437bf14
sofilago <sofia.lago.93@gmail.com>
2020-09-01 23:40:23 -0300

    Iconos lindos

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -46,35 +46,16 @@ export default function Landing () {
 </div>
 
 <footer class="page-footer font-small special-color-dark pt-4">
-  <div class="container">
-    <ul class="list-unstyled list-inline text-center">
-      <li class="list-inline-item">
-        <a class="btn-floating btn-fb mx-1">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-tw mx-1">
-          <i class="fab fa-twitter"></i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-gplus mx-1">
-          <i class="fab fa-google-plus-g"></i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-li mx-1">
-          <i class="fab fa-linkedin-in"></i>
-        </a>
-      </li>
-      <li class="list-inline-item">
-        <a class="btn-floating btn-dribbble mx-1">
-          <i class="fab fa-dribbble"></i>
-        </a>
-      </li>
+  <div class="redes-container">
+    <ul>
+      <li><a class="btn-floating btn-fb mx-1"><i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"></i></a></li>
+      <li><a class="btn-floating btn-tw mx-1"><i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"></i></a></li>
+      <li><a class="btn-floating btn-gplus mx-1"><i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"></i></a></li>
+      <li><a class="btn-floating btn-li mx-1"><i class="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"></i></a></li>
+      <li><a class="btn-floating btn-li mx-1"><i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"></i></a></li>
     </ul>
   </div> 
+
   <div class="footer-copyright text-center py-3">
     <a href="https://mdbootstrap.com/"> Universo Verde</a>
   </div>
